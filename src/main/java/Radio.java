c7cd1a8524adaa97714dd5b7ffc2a628706bc087
TimmyLi <artelaw@gmail.com>
2022-09-21 22:42:52 +0600

    Добавление плагина lombook и его проверка.

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,3 +1,11 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
     private int numberOfStation;
     private int volume;
@@ -6,76 +14,4 @@ public class Radio {
     private int minVolume;
     private int maxStation = maxNumberOfStation;
     private int minStation;
-
-    public Radio(int maxNumberOfStation) {
-        this.maxNumberOfStation = maxNumberOfStation;
-        maxStation = this.maxNumberOfStation - 1;
-    }
-
-    public Radio(){
-        maxNumberOfStation = 10;
-        maxStation = maxNumberOfStation - 1;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public int getNumberOfStation() {
-        return numberOfStation;
-    }
-
-    public void setVolume(int newVolume) {
-        if (newVolume > maxVolume) {
-            volume = maxVolume;
-        } else if (newVolume < minVolume) {
-            volume = minVolume;
-        } else {
-            volume = newVolume;
-        }
-    }
-
-    public void setVolumeUp() {
-        if (volume + 1 >= maxVolume) {
-            volume = maxVolume;
-        } else {
-            volume = volume + 1;
-        }
-
-    }
-
-    public void setVolumeDown() {
-        if (volume - 1 <= minVolume) {
-            volume = minVolume;
-        } else {
-            volume = volume - 1;
-        }
-    }
-
-    public void setNumberOfStation(int newNumber) {
-        if (newNumber > maxStation) {
-            return;
-        } else if (newNumber < minStation) {
-            return;
-        } else {
-            numberOfStation = newNumber;
-        }
-    }
-
-    public void setNumberOfStationNext() {
-        if (numberOfStation + 1 > maxStation) {
-            numberOfStation = minStation;
-        } else {
-            numberOfStation = numberOfStation + 1;
-        }
-    }
-
-    public void setNumberOfStationPrev() {
-        if (numberOfStation - 1 < minStation) {
-            numberOfStation = maxStation;
-        } else {
-            numberOfStation = numberOfStation - 1;
-        }
-
-    }
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -3,126 +3,14 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
     @Test
+    public void setMaxVolume (){
+    Radio radio = new Radio();
+    radio.setMaxVolume(5);
 
-    public void setVolumeAboveMaxTest() {
-        Radio radio = new Radio();
-        radio.setVolume(100);
+    int expected = 5;
+    int actual = radio.getMaxVolume();
 
-        int expected = 100;
-        int actual = radio.getVolume();
+    Assertions.assertEquals(expected,actual);
 
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setVolumeBelowMinTest() {
-        Radio radio = new Radio();
-        radio.setVolume(-5);
-
-        int expected = 0;
-        int actual = radio.getVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setVolumeToTheMinTest() {
-        Radio radio = new Radio();
-        radio.setVolume(0);
-
-        int expected = 0;
-        int actual = radio.getVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setVolumeToTheMaxTest() {
-        Radio radio = new Radio();
-        radio.setVolume(10);
-
-        int expected = 10;
-        int actual = radio.getVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setVolumeTheLessOneToMinTest() {
-        Radio radio = new Radio();
-        radio.setVolume(0);
-        radio.setVolumeDown();
-
-        int expected = 0;
-        int actual = radio.getVolume();
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void setVolumeTheMoreOneToMaxTest() {
-        Radio radio = new Radio();
-        radio.setVolume(100);
-        radio.setVolumeUp();
-
-        int expected = 100;
-        int actual = radio.getVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void upAndDownVolumeTest() {
-        Radio radio = new Radio();
-        radio.setVolume(2);
-        radio.setVolumeUp();
-        radio.setVolumeDown();
-
-        int expected = 2;
-        int actual = radio.getVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nextAndPrevRadioStationTest() {
-        Radio radio = new Radio();
-        radio.setNumberOfStation(9);
-        radio.setNumberOfStationNext();
-        radio.setNumberOfStationPrev();
-        radio.setNumberOfStation(5);
-        radio.setNumberOfStationNext();
-        radio.setNumberOfStationPrev();
-
-        int expected = 5;
-        int actual = radio.getNumberOfStation();
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void setMaxAndMinStationTest(){
-        Radio radio = new Radio();
-        radio.setNumberOfStation(10);
-        radio.setNumberOfStation(-1);
-        int expected = 0;
-        int actual = radio.getNumberOfStation();
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void setMaxNumberOfStation(){
-        Radio radio = new Radio(100);
-        radio.setNumberOfStation(99);
-        radio.setNumberOfStationNext();
-
-        int expected = 0;
-        int actual = radio.getNumberOfStation();
-
-        Assertions.assertEquals(expected,actual);
     }
 }
