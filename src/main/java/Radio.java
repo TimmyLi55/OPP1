13e2bc0688884411361990273ee9b7e45c746605
TimmyLi <artelaw@gmail.com>
2022-09-30 11:56:16 +0600

    Доработка геттеров и сеттеров через lombook

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -14,4 +14,60 @@ public class Radio {
     private int minVolume;
     private int maxStation = maxNumberOfStation;
     private int minStation;
+
+    public void setVolume(int newVolume) {
+        if (newVolume > maxVolume) {
+            volume = maxVolume;
+        } else if (newVolume < minVolume) {
+            volume = minVolume;
+        } else {
+            volume = newVolume;
+        }
+    }
+
+    public void setVolumeUp() {
+        if (volume + 1 >= maxVolume) {
+            volume = maxVolume;
+        } else {
+            volume = volume + 1;
+        }
+
+    }
+
+    public void setVolumeDown() {
+        if (volume - 1 <= minVolume) {
+            volume = minVolume;
+        } else {
+            volume = volume - 1;
+        }
+    }
+
+    public void setNumberOfStation(int newNumber) {
+        if (newNumber > maxStation) {
+            return;
+        } else if (newNumber < minStation) {
+            return;
+        } else {
+            numberOfStation = newNumber;
+        }
+    }
+
+    public void setNumberOfStationNext() {
+        if (numberOfStation + 1 > maxStation) {
+            numberOfStation = minStation;
+        } else {
+            numberOfStation = numberOfStation + 1;
+        }
+    }
+
+    public void setNumberOfStationPrev() {
+        if (numberOfStation - 1 < minStation) {
+            numberOfStation = maxStation;
+        } else {
+            numberOfStation = numberOfStation - 1;
+        }
+
+    }
 }
+
+
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -3,14 +3,14 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
     @Test
-    public void setMaxVolume (){
-    Radio radio = new Radio();
-    radio.setMaxVolume(5);
+    public void setMaxVolume() {
+        Radio radio = new Radio();
+        radio.setMaxVolume(5);
 
-    int expected = 5;
-    int actual = radio.getMaxVolume();
+        int expected = 5;
+        int actual = radio.getMaxVolume();
 
-    Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
 
     }
 }
